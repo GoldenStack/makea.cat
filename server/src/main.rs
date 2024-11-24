9ae10a60d617bf8c7d9704a68a19172a645e64fc
GoldenStack <git@goldenstack.net>
2024-11-23 19:35:57 -0600

    Make code a little cleaner

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,6 @@
 
 use core::f32;
-use std::{f32::consts::PI, io::BufWriter};
+use std::{f32::consts::PI, io::BufWriter, sync::OnceLock};
 
 use anyhow::Result;
 use axum::{body::Body, http::{header::CONTENT_TYPE, Request, StatusCode}, response::{IntoResponse, Redirect}, routing::get, Router};
@@ -53,124 +53,7 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-fn draw_cat(dt: &mut DrawTarget, base: &Transform) {
-
-    let mut rng = rand::thread_rng();
-
-    let stroke = &StrokeStyle {
-        cap: LineCap::Round,
-        join: LineJoin::Miter,
-        width: 5.,
-        miter_limit: 2.,
-        dash_array: Vec::new(),
-        dash_offset: 0.,
-    };
-
-    let black = &Source::Solid(SolidSource {
-        r: 0x0,
-        g: 0x0,
-        b: 0x0,
-        a: 0xff,
-    });
-
-    fn random_color<'a>() -> Source<'a> {
-        let mut rng = rand::thread_rng();
-        Source::Solid(SolidSource {
-            r: rng.gen_range(100..=255),
-            g: rng.gen_range(100..=255),
-            b: rng.gen_range(100..=255),
-            a: 0xff,
-        })
-    }
-
-    let tail = {
-        let mut pb = PathBuilder::new();
-
-        let (x, y) = (60., 0.);
-        
-        let sign = if rng.gen::<bool>() { 1. } else { -1. };
-
-        pb.move_to(x, y);
-        if rng.gen::<bool>() {
-            let scale = rng.gen_range(2.5..3.5);
-
-            pb.cubic_to(
-                x + scale*rng.gen_range(12.0..17.0), y + scale*sign*rng.gen_range(0.0..5.0),
-                x + scale*rng.gen_range(-5.0..0.0), y + scale*sign*rng.gen_range(10.0..15.0),
-                x + scale*rng.gen_range(15.0..25.0), y + scale*sign*rng.gen_range(5.0..15.0),
-            );
-        } else {
-            let scale = rng.gen_range(3.0..4.0);
-
-            pb.quad_to(
-                x + scale*rng.gen_range(12.0..17.0), y + scale*sign*rng.gen_range(0.0..5.0),
-                x + scale*rng.gen_range(5.0..20.0), y + scale*sign*rng.gen_range(12.0..17.0),
-            );
-        }
-
-        pb.finish()
-    };
-
-    let neck = {
-        let mut pb = PathBuilder::new();
-        pb.rect(-15., -15., 30., 30.);
-        pb.close();
-
-        pb.finish()
-    };
-
-    let body = {
-        let mut pb = PathBuilder::new();
-        ellipse(&mut pb, 0., 0., 60., 27.);
-        pb.close();
-
-        pb.finish()
-    };
-
-    let legs = {
-        let mut pb = PathBuilder::new();
-
-        ellipse(&mut pb, 0., 0., 7., 25.);
-
-        pb.finish()
-    };
-
-    dt.set_transform(&base);
-    
-    dt.stroke(&tail, &black, &StrokeStyle {
-        cap: LineCap::Round,
-        join: LineJoin::Miter,
-        width: 7.,
-        miter_limit: 2.,
-        dash_array: Vec::new(),
-        dash_offset: 0.,
-    }, &DrawOptions::new());
-
-    dt.set_transform(&Transform::rotation(Angle::degrees(-30.)).then_translate(Vector::new(-45., -19.)).then(&base));
-    dt.stroke(&neck, &black, &stroke, &DrawOptions::new());
-    dt.fill(&neck, &random_color(), &DrawOptions::new());
-
-    dt.set_transform(&Transform::rotation(Angle::degrees(20.)).then_translate(Vector::new(-45., 21.)).then(&base));
-    dt.stroke(&legs, &black, &stroke, &DrawOptions::new());
-    dt.fill(&legs, &random_color(), &DrawOptions::new());
-
-    dt.set_transform(&Transform::rotation(Angle::degrees(5.)).then_translate(Vector::new(-25., 26.)).then(&base));
-    dt.stroke(&legs, &black, &stroke, &DrawOptions::new());
-    dt.fill(&legs, &random_color(), &DrawOptions::new());
-
-    dt.set_transform(&Transform::rotation(Angle::degrees(-5.)).then_translate(Vector::new(25., 26.)).then(&base));
-    dt.stroke(&legs, &black, &stroke, &DrawOptions::new());
-    dt.fill(&legs, &random_color(), &DrawOptions::new());
-
-    dt.set_transform(&Transform::rotation(Angle::degrees(-20.)).then_translate(Vector::new(45., 21.)).then(&base));
-    dt.stroke(&legs, &black, &stroke, &DrawOptions::new());
-    dt.fill(&legs, &random_color(), &DrawOptions::new());
-    dt.set_transform(&base);
-    
-    dt.stroke(&body, &black, &stroke, &DrawOptions::new());
-    dt.fill(&body, &random_color(), &DrawOptions::new());
-
-
+fn draw_head(dt: &mut DrawTarget) {
     let ears = {
         let mut pb = PathBuilder::new();
 
@@ -228,19 +111,112 @@ fn draw_cat(dt: &mut DrawTarget, base: &Transform) {
         pb.finish()
     };
 
-    dt.set_transform(&Transform::translation(-59., -44.).then(&base));
-
-    dt.stroke(&ears, &black, &stroke, &DrawOptions::new());
-    dt.fill(&ears, &random_color(), &DrawOptions::new());
+    dt.stroke(&ears, &BLACK, &stroke(), &DRAW);
+    dt.fill(&ears, &random_color(), &DRAW);
     
-    dt.stroke(&head, &black, &stroke, &DrawOptions::new());
-    dt.fill(&head, &random_color(), &DrawOptions::new());
+    dt.stroke(&head, &BLACK, &stroke(), &DRAW);
+    dt.fill(&head, &random_color(), &DRAW);
 
-    dt.fill(&eyes, &black, &DrawOptions::new());
+    dt.fill(&eyes, &BLACK, &DRAW);
 
-    dt.fill(&nose, &black, &DrawOptions::new());
+    dt.fill(&nose, &BLACK, &DRAW);
+}
 
-    dt.set_transform(&Transform::identity());
+fn draw_cat(dt: &mut DrawTarget, base: &Transform) {
+    let mut rng = rand::thread_rng();
+
+    let tail = {
+        let mut pb = PathBuilder::new();
+
+        let (x, y) = (60., 0.);
+        
+        let sign = if rng.gen::<bool>() { 1. } else { -1. };
+
+        pb.move_to(x, y);
+        if rng.gen::<bool>() {
+            let scale = rng.gen_range(2.5..3.5);
+
+            pb.cubic_to(
+                x + scale*rng.gen_range(12.0..17.0), y + scale*sign*rng.gen_range(0.0..5.0),
+                x + scale*rng.gen_range(-5.0..0.0), y + scale*sign*rng.gen_range(10.0..15.0),
+                x + scale*rng.gen_range(15.0..25.0), y + scale*sign*rng.gen_range(5.0..15.0),
+            );
+        } else {
+            let scale = rng.gen_range(3.0..4.0);
+
+            pb.quad_to(
+                x + scale*rng.gen_range(12.0..17.0), y + scale*sign*rng.gen_range(0.0..5.0),
+                x + scale*rng.gen_range(5.0..20.0), y + scale*sign*rng.gen_range(12.0..17.0),
+            );
+        }
+
+        pb.finish()
+    };
+
+    let neck = {
+        let mut pb = PathBuilder::new();
+        pb.rect(-15., -15., 30., 30.);
+        pb.close();
+
+        pb.finish()
+    };
+
+    let body = {
+        let mut pb = PathBuilder::new();
+        ellipse(&mut pb, 0., 0., 60., 27.);
+        pb.close();
+
+        pb.finish()
+    };
+
+    let leg = {
+        let mut pb = PathBuilder::new();
+
+        ellipse(&mut pb, 0., 0., 7., 25.);
+
+        pb.finish()
+    };
+
+    dt.set_transform(&base);
+    
+    dt.stroke(&tail, &BLACK, &StrokeStyle {
+        cap: LineCap::Round,
+        join: LineJoin::Miter,
+        width: 7.,
+        miter_limit: 2.,
+        dash_array: Vec::new(),
+        dash_offset: 0.,
+    }, &DRAW);
+
+    dt.set_transform(&Transform::rotation(Angle::degrees(-30.)).then_translate(Vector::new(-45., -19.)).then(&base));
+    dt.stroke(&neck, &BLACK, &stroke(), &DRAW);
+    dt.fill(&neck, &random_color(), &DRAW);
+
+    let legs = [
+        ((-45., 21.), 20.),
+        ((-25., 26.), 5.),
+        (( 25., 26.), -5.),
+        (( 45., 21.), -20.),
+    ];
+
+    for ((x, y), rot) in legs {
+        let translation = Transform::rotation(Angle::degrees(rot)).then_translate(Vector::new(x, y));
+
+        dt.set_transform(&translation.then(&base));
+        dt.stroke(&leg, &BLACK, &stroke(), &DRAW);
+        dt.fill(&leg, &random_color(), &DRAW);
+    }
+
+    dt.set_transform(&base);
+    
+    dt.stroke(&body, &BLACK, &stroke(), &DRAW);
+    dt.fill(&body, &random_color(), &DRAW);
+
+    // Draw head at (-59, 44).
+    dt.set_transform(&Transform::translation(-59., -44.).then(&base));
+    draw_head(dt);
+    dt.set_transform(&base);
+
 
     // Colon three :3
     // {
@@ -288,8 +264,45 @@ fn draw_cat(dt: &mut DrawTarget, base: &Transform) {
     //         dash_offset: 0.,
     //     };
 
-    //     dt.stroke(&path, &black, &style, &DrawOptions::new());
+    //     dt.stroke(&path, &BLACK, &style, &DRAW);
     // }
+}
+
+fn stroke() -> &'static StrokeStyle {
+    static STROKE: OnceLock<StrokeStyle> = OnceLock::new();
+    STROKE.get_or_init(|| {
+        StrokeStyle {
+            cap: LineCap::Round,
+            join: LineJoin::Miter,
+            width: 5.,
+            miter_limit: 2.,
+            dash_array: Vec::new(),
+            dash_offset: 0.,
+        }
+    })
+}
+
+const BLACK: Source = Source::Solid(SolidSource {
+    r: 0x0,
+    g: 0x0,
+    b: 0x0,
+    a: 0xff,
+});
+
+const DRAW: DrawOptions = DrawOptions {
+    blend_mode: BlendMode::SrcOver,
+    alpha: 1.,
+    antialias: AntialiasMode::Gray,
+};
+
+fn random_color<'a>() -> Source<'a> {
+    let mut rng = rand::thread_rng();
+    Source::Solid(SolidSource {
+        r: rng.gen_range(100..=255),
+        g: rng.gen_range(100..=255),
+        b: rng.gen_range(100..=255),
+        a: 0xff,
+    })
 }
 
 fn ellipse(pb: &mut PathBuilder, x: f32, y: f32, width: f32, height: f32) {
